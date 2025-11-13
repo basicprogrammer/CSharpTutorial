679563a22ae201dfa5aaee1d9fae728a4bbe45cd
Baran AKDENİZ <baranakdeniz_35@hotmail.com>
2025-11-13 17:20:18 +0300

    part2

---
+++ CSharpTutorial/Metotlar/Metotlar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    public class MathHelper
+    {
+
+        public static int Square(int number)
+        {
+            return number * number;
+        }
+    
+   
+   
+        static void Main()
+        {
+
+            int result = MathHelper.Square(5);
+            Console.WriteLine(result);
+
+
+
+        }
+
+    }
+}
