d3f61d13c10c82f860d15f18d867b97c4ba025c3
Baran AKDENİZ <baranakdeniz_35@hotmail.com>
2025-11-13 13:17:06 +0300

    Tutorial1

---
+++ CSharpTutorial/CSharpTutorial/VeriTipleri.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial
+{
+    internal class VeriTipleri
+    {
+        static void Main()
+        {
+            int tamSayi = -2147483648;               // 32 bit işaretli tam sayı.
+                                                     // -2,147,483,648 ile 2,147,483,647 arasında      
+
+
+            long isikYili = 9223372036854775807;  // 64 bit işaretli tam sayı.
+                                                  // -9,223,372,036,854,775,808 ile 9,223,372,036,854,775,807 arasında
+
+
+            short alınanYol = 32767;                   // 16 bit işaretli tam sayı.
+                                                       // -32,768 ile 32,767 arasında
+
+
+            byte yas = 50;                              // byte: 8 bit işaretsiz tam sayı.
+                                                        // 0 ile 255 arasında
+
+
+            float pi = 3.14f;                            // 32 bit ondalıklı sayı.
+                                                         // Yaklaşık olarak ±1.5 x 10−45 ile ±3.4 x 1038 (3.402823e38 ile 3.402823e38 arasında)
+                                                         // Virgülden sonra 7 basamağa kadar hassasiyet   
+
+
+            double uzunluk = 20.4;                      // 64 bit ondalıklı sayı.
+                                                        // Yaklaşık olarak ±5.0 × 10−324 ile ±1.7 × 10308 (1.79769313486232e308 ile 1.79769313486232e308 arasında)
+                                                        // Virgülden sonra 15-16 basamağa kadar hassasiyet  
+
+            char basHarfi = 'G';                        // 16 bit tek karakter saklamak için kullanılır. Tek tırnak içinde tanımlanır.  
+
+            string ad = "Selami";                        // Metin verilerini temsil eden veri tipi. Çift tırnak işareti ile yazılır.                    
+
+            Boolean boolValue = true;                  // true (doğru) veya false (yanlış) değerlerini alabilir. Mantıksal işlemler için kullanılır. 
+
+            //Console.WriteLine("Tam Sayı: " + tamSayi+ "Long"+isikYili+ "short"+ alınanYol + "byte"+ yas + "float"+ pi + "double"+ uzunluk + "char"+ basHarfi + "string"+ ad + "Boolean" + boolValue);
+
+            // $ işareti ile string interpolation kullanma
+            Console.WriteLine($"Tam Sayı: {tamSayi}, Long: {isikYili}, short: {alınanYol}, byte: {yas}, float: {pi}, double: {uzunluk}, char: {basHarfi}, string: {ad}, Boolean: {boolValue}");
+        }
+    }
+    }
+
---
+++ CSharpTutorial/SartBloklari/SartBloklari.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SartBloklari
+{
+    internal class SartBloklari
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2;
+            Console.Write("Birinci sayıyı girin.");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı girin.");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı girin.");
+
+            if (sayi1 > sayi2)
+            {
+                Console.WriteLine("1. sayı olan {0} daha büyük.", sayi1);
+            }
+            else
+            {
+                Console.WriteLine("2. sayı olan {0} daha büyük.", sayi2);
+
+            }
+
+
+
+            char harf;
+            Console.Write("Bir karakter giriniz : ");
+            harf = Convert.ToChar(Console.ReadLine());
+            switch (harf)
+            {
+                case 'a': Console.WriteLine("Sesli Harf"); break;
+                case 'e': Console.WriteLine("Sesli Harf"); break;
+                case 'u': Console.WriteLine("Sesli Harf"); break;
+                case 'ü': Console.WriteLine("Sesli Harf"); break;
+                case 'ı': Console.WriteLine("Sesli Harf"); break;
+                case 'i': Console.WriteLine("Sesli Harf"); break;  
+                case 'o': Console.WriteLine("Sesli Harf"); break;
+                case 'ö': Console.WriteLine("Sesli Harf"); break;
+                default:
+                    Console.WriteLine("\nGirilen karakter sessiz bir harf");
+                    break;
+            }
+        }
+    }
+}
