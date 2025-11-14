02bf69acfd297495645a9b2781b4b362d38c11da
Baran AKDENİZ <baranakdeniz_35@hotmail.com>
2025-11-14 13:52:39 +0300

    Part3

---
+++ CSharpTutorial/Diziler/Diziler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler
+{
+    internal class Diziler
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5] { "mavi", "kırmızı", "sarı", "yeşil", "turuncu" };
+
+            foreach (var renk in renkler)
+            {
+                Console.WriteLine(renk);
+            }
+            Console.WriteLine("Dizilerin uzunluğu: " + renkler.Length);
+
+
+
+
+
+
+            string[] hayvanlar = { "kedi", "köpek", "kuş", "maymun", "tavşan" };
+
+            foreach (var hayvan in hayvanlar)
+            {
+                Console.WriteLine(hayvan);
+            }
+            Console.WriteLine("Dizilerin uzunluğu: " + hayvanlar.Length);
+
+
+
+
+
+            string[] bolgeler= new string[5]; // 5 elemanlı boş bir dizi tanımlandı.
+            bolgeler[0] = "İç Anadolu";       // Dizide elemanlar tek tek atandı. Atanmayanlar null olarak kalır ve yansırlar.
+            bolgeler[1] = "Marmara";
+            bolgeler[2] = "Ege";
+
+            foreach (var bolge in bolgeler)
+            {
+                Console.WriteLine(bolge);
+            }
+            Console.WriteLine("Dizilerin uzunluğu: " + bolgeler.Length);    
+        }
+    }
+}
--- CSharpTutorial/Metotlar/Metotlar.cs
+++ CSharpTutorial/Metotlar/Metotlar.cs
@@ -13,17 +13,51 @@ namespace Metotlar
         {
             return number * number;
         }
-    
-   
-   
+
+
+
+        static int refAdd(ref int a, int b) // Ref yerine out da kullanılabilirdi.
+                                            // Ref yerine out da kullanılabilirdi lakin out kullanıldığında a değişkenine metodun içinde bir değer ataması yapılması zorunludur.
+        {
+            a = 2; //Reference olarak gönderilen değişkenin değeri değişir.
+            var resultref=a + b; 
+            return resultref;
+            // var ve return kullanımı yerine direkt olarak return a + b; da yazılabilirdi.
+        }
+
+        static int notrefAdd(int c, int d)
+        {
+            c = 1; //Değer olarak gönderilen değişkenin değeri değişmez.Referans olarak gönderilmediği için orijinal değişken etkilenmez.
+            var resultnotref = c + d;
+            return resultnotref;
+
+        }
+
         static void Main()
         {
-
-            int result = MathHelper.Square(5);
+            // Square metodunu çağırma
+            int result = MathHelper.Square(5); // Square metoduna 5 sayısını gönderiyoruz.
             Console.WriteLine(result);
+            //---------------------------------------------
 
 
 
+
+            // Metotlarda Ref ve Out kullanımı
+            int a = 5;
+            int b = 10;
+
+            var resultref = refAdd(ref a, b); // a değişkeni ref olarak gönderildiği için değeri değişebilir.
+                                              // Ref yerine out da kullanılabilirdi lakin out kullanıldığında a değişkenine metodun içinde bir değer ataması yapılması zorunludur.
+            Console.WriteLine(resultref);
+
+
+
+            int c = 4;
+            int d = 8;           
+
+            var resultnotref = notrefAdd(c, d);
+            Console.WriteLine(resultnotref);  
         }
 
     }
