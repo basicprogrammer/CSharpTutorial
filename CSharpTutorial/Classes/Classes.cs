17b92c2fa735db8894cfd21b6d330e5d0e914223
Baran AKDENİZ <baranakdeniz_35@hotmail.com>
2025-11-17 14:41:10 +0300

    Part3

---
+++ CSharpTutorial/Classes/Classes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Classes
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager(); // instance - örnek
+            customerManager.Add();
+            customerManager.Update();
+
+
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "X";
+            customer.LastName = "Y";
+
+            Customer customer2= new Customer
+            {
+                Id = 2,
+                FirstName = "A",
+                LastName = "B"
+            };
+            Console.WriteLine($"Musteri ID:{ customer2.Id}, Ad:{ customer2.FirstName}, Soyad:{ customer2.LastName}");  // get bloğundan dolayı sadece okunabilir        
+            Console.ReadLine();
+        }
+    }
+        
+}
--- CSharpTutorial/Stringler/Stringler.cs
+++ CSharpTutorial/Stringler/Stringler.cs
@@ -19,8 +19,18 @@ namespace Stringler
 
 
             string city2 = "İstanbul";
-            string result = city + " " + city2;
-            Console.Write(result);
+            Console.WriteLine(String.Format("{0}{1}",city,city2));
+            Console.WriteLine(city.Length);
+            Console.WriteLine(city.Clone());
+            bool result = city.EndsWith("ğ");
+            bool result2 = city.StartsWith("ğ");
+            var result3 = city.IndexOf("k"); // k harfinin indexini verir, bulamazsa -1 döner
+            var result4 = city.LastIndexOf("a"); // sondan başlayarak a harfinin indexini verir 
+            var result5 = city.Insert(0, "Hello "); // başa ekleme yapar
+            Console.WriteLine(result5);
+            var result6 = city.Substring(2,2); // 2. indexten başla 2 karakter al
+            Console.WriteLine(result6);
+
             Console.ReadLine();
         }
 
